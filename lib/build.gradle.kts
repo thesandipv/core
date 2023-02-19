0bf9fe3ee6cf28052d39979c8c2fc2afe233fd9d
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2023-02-19 20:26:16 +0530

    compileOptions: java 11

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -46,14 +46,14 @@ tasks.dokkaHtml.configure {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion")
 
-    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.5.5")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.transition:transition:1.4.1")
     implementation("com.afollestad.material-dialogs:core:3.3.0")
-    implementation("com.google.android.material:material:1.8.0-beta01")
+    implementation("com.google.android.material:material:1.9.0-alpha01")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.jakewharton.timber:timber:5.0.1")
 }
